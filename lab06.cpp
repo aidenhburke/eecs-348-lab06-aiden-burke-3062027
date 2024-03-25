b4a5f0b4529e9003817fe0eddb26e330b4a83bc4
aidenhburke <aidenburke@ku.edu>
2024-03-24 20:22:14 -0500

    Update lab06.cpp
    Completed main() to print out the proper outputs of each problem

--- lab06.cpp
+++ lab06.cpp
@@ -36,6 +36,7 @@ void print_matrix(int matrix[size][size]){
         };
         cout << "\n";
     }
+    cout << "\n";
 };
 void add_matrix(int m1[size][size], int m2[size][size], int result[size][size]){
     for(int i=0; i < size; i++){
@@ -60,11 +61,21 @@ void subtract_matrix(int m1[size][size], int m2[size][size], int result[size][si
         }
     }
 };
-void update_matrix(int matrix[size][size], int row, int col, int val){
+void update_matrix(int matrix[size][size]){
+    while(1){
+    int row, col, n;
+    cout << "Row of matrix 1 you want to update? (Assume row counting begins at 0): ";
+    cin >> row;
+    cout << "Column of matrix 1 you want to update? (Assume column counting begins at 0): ";
+    cin >> col;
+    cout << "What value do you want to put in row " << row << ", column " << col << " of matrix 1?: ";
+    cin >> n;
     if(row < size & col < size){
-        matrix[row][col] = val;
+        matrix[row][col] = n;
+        break;
     } else {
-        cout << "Invalid row/column";
+        cout << "Invalid row/column.\n";
+    }
     }
 };
 void get_max(int matrix[size][size]){
@@ -76,7 +87,7 @@ void get_max(int matrix[size][size]){
             }
         }
     }
-    cout << "Max value of matrix 1: " << max;
+    cout << "Max value of matrix 1: " << max << "\n\n";
 };
 void transpose(int matrix[size][size], int result[size][size]){
     for(int i=0; i < size; i++){
@@ -87,13 +98,42 @@ void transpose(int matrix[size][size], int result[size][size]){
 };
 
 int main() {
-    int matrix1[size][size] = {{1,2,3},{4,5,6},{7,8,9}};
-    int matrix2[size][size] = {{2,3,4},{5,6,7},{8,9,10}};
 
-    int result[size][size];
-    multiply_matrix(matrix1,matrix2,result);
+    int matrix1[size][size] = {{1,2,3},{4,5,6},{7,8,9}}; // make these read from file
+    int matrix2[size][size] = {{2,3,4},{5,6,7},{8,9,10}}; // make these read from file
+
+    // read size, matrix 1, and matrix 2 from file
+
+    cout << "Matrix 1:\n";
     print_matrix(matrix1);
-    print_matrix(result);
+    cout << "Matrix 2:\n";
+    print_matrix(matrix2);
+
+    int addition_result[size][size];
+    add_matrix(matrix1, matrix2, addition_result);
+    cout << "Matrix 1 + Matrix 2:\n";
+    print_matrix(addition_result);
+
+    int multiplication_result[size][size];
+    multiply_matrix(matrix1, matrix2, multiplication_result);
+    cout << "Matrix 1 * Matrix 2:\n";
+    print_matrix(multiplication_result);
+
+    int subtraction_result[size][size];
+    subtract_matrix(matrix1, matrix2, subtraction_result);
+    cout << "Matrix 1 - Matrix 2:\n";
+    print_matrix(subtraction_result);
+
+    update_matrix(matrix1);
+    cout << "Updated matrix 1:\n";
+    print_matrix(matrix1);
+
+    get_max(matrix1);
+
+    int transpose_result[size][size];
+    transpose(matrix1, transpose_result);
+    cout << "Transposed Matrix 1:\n";
+    print_matrix(transpose_result);
 
     return 0;
 };
